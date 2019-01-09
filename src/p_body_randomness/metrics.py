42c701f156f7d5ea0afd568d40aa34054e8f448e
marydmit <mdmit@eduroam-hg-dock-1-389.ethz.ch>
2019-01-09 13:07:20 +0100

    Added nearest neighbor distance calculation to the metrics script.

--- src/p_body_randomness/metrics.py
+++ src/p_body_randomness/metrics.py
@@ -2,12 +2,41 @@
 Calculate different metrics.
 '''
 
+def eucledian_distance(coordinates1 : list, coordinates2 : list):
+    '''
+    Function: calculates Eucledian distance.
+    Input:    two lists of coordinates of equal length
+    Output:   the calculated distance.
+    '''
+    distanceSum = 0
+    for i in range(len(coordinates1)):
+        distanceSum += (coordinates1[i] - coordinates2[i])**2
+    return np.sqrt(distanceSum)
 
-def nearest_neighbor_distance(p_bodies):
+def nearest_neighbor_distance(pBodylist : list):
     '''
-    Args:
-        p_bodies: list of (x,y) tuples
-    Returns:
-        (mean, std): the mean and standard deviation of the nnds of all p_bodies
+    Function: for each p-body in a list, calculates the distance to its nearest neighbor.
+    Input:    a list of tuples, where a tuple is of format: (xCoord, yCoord, area) for each p-body.
+    Output:   a list of distances to nearest neighbors.
     '''
-    raise NotImplemented
+
+    # Creating a distance matrix.
+    pairwiseDistances = np.zeros((len(pBodylist), len(pBodylist)))
+    
+    # Going through all p-bodies one by one.
+    for i in range(len(pBodylist)-1):
+        for j in range(i+1, len(pBodylist)):
+            # Calculating Eucledian distance.
+            distance = eucledian_distance(pBodylist[i][0:-1], pBodylist[j][0:-1])
+            # Filling up distance matrix.
+            pairwiseDistances[i][j] = distance
+            pairwiseDistances[j][i] = distance
+    
+    # List containig all minimal distances.
+    nearestNeighbors = []
+    # Selecting minimum distances.
+    for pBody in pairwiseDistances:
+        nearestNeighbors.append(min(pBody[pBody > 0]))
+    
+    # Returning list of distances to the nearest neighbors.
+    return nearestNeighbors
