d00a505228176bb6240adc7dec6238b63c9b9878
Joel Lüthi <joel.luethi@uzh.ch>
2019-01-11 09:16:25 +0100

    Fixing bug in protein_signal_calculation & adding results of volume sampling run

--- src/p_body_randomness/protein_signal_calculation.py
+++ src/p_body_randomness/protein_signal_calculation.py
@@ -16,30 +16,30 @@ def calculate_mean_intensities(protein_signal, pbody_mask, cell_mask, nucleus_im
     # Creating mask for the nucleus
     nucleus_smoothed = cv2.GaussianBlur(nucleus_image, (5,5), 0)
     nucleus_mask = nucleus_smoothed > 10
-    
+
     # Removing small p-bodies from the image.
     small_p_body_kernel = np.ones((3, 3), np.uint8)
-    no_small_p_bodies = cv2.morphologyEx(pBodies, cv2.MORPH_OPEN, small_p_body_kernel)
+    no_small_p_bodies = cv2.morphologyEx(pbody_mask, cv2.MORPH_OPEN, small_p_body_kernel)
 
     # Dilating remaining p-bodies.
     dilation_kernel = np.ones((10, 10), np.uint8)
     dilated_pbodies = cv2.dilate(no_small_p_bodies, dilation_kernel)
-    
+
     # Removing resulting areas that are not in the cytoplasm.
     dilated_pbodies[(cell_mask == 0) | (nucleus_mask != 0)] = 0
-    
+
     # Determining which pixels are in p-body surroundings.
     p_body_surroundings = dilated_pbodies - pbody_mask > 0
-    
+
     # Processing the image to make the protein signal smoother.
     proteins_smooth = smooth_protein_image(protein_signal, pbody_mask, cell_mask, nucleus_image)
-    
+
     # Calculating average signal in the p-body surroundings.
     surroundings_signal = proteins_smooth[p_body_surroundings]
     surr_mean = np.mean(surroundings_signal)
-    
+
     # Calculating average signal in the cytoplasm.
     cytoplasm_signal = proteins_smooth[(cell_mask != 0) & (nucleus_mask == 0)]
     cyto_mean = np.mean(cytoplasm_signal)
-    
+
     return (cyto_mean, surr_mean)
