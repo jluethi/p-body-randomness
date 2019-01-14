12da5db1dfa10fd2ea9397904e734b6b0d99ceab
Joel Lüthi <joel.luethi@uzh.ch>
2019-01-14 17:31:44 +0100

    Add script to run improved area sampling: shrunken nucleus and min sampling distance

--- src/p_body_randomness/smooth_image.py
+++ src/p_body_randomness/smooth_image.py
@@ -14,19 +14,19 @@ def smooth_protein_image(protein_signal, pbody_mask, cell_mask, nucleus_image, n
     # We first substract some signal in areas where p-bodies are located.
     no_p_bodies = 1 * protein_signal
     no_p_bodies[pbody_mask != 0] = no_p_bodies[pbody_mask != 0] * 0.75
-    
+
     # We create a mask for the nucleus
     nucleus_smoothed = cv2.GaussianBlur(nucleus_image,(5,5),0)
     nucleus_mask = nucleus_smoothed > nucleus_threshold
-    
+
     # We extract only the cytoplasmic signal
     cytoplasm = 1 * no_p_bodies
     cytoplasm[(cell_mask == 0) | (nucleus_mask != 0)] = 0
-    
+
     # Calculating total signal from the cytoplasm and nucleus area.
     total_cytoplasm = np.sum(cytoplasm)
     nucleus_area = np.sum(nucleus_mask != 0)
-    
+
     # Making sure nucleus intensity is a certain percentage from the overall signal.
     cytoplasm[nucleus_mask != 0] = (nucleus_percentage * total_cytoplasm)/(nucleus_area * (1 - nucleus_percentage))
 
@@ -34,9 +34,31 @@ def smooth_protein_image(protein_signal, pbody_mask, cell_mask, nucleus_image, n
     smooth_array = 1 * cytoplasm
     for i in range(5):
         smooth_array = cv2.GaussianBlur(smooth_array, (9, 9), 0)
-    
+
     # Finally, restricting signal to area within the cell mask.
     final_image = 1 * smooth_array
     final_image[cell_mask == 0] = 0
-    
+
     return final_image
+
+def add_nuclear_probability(cell_mask, nucleus_image, nucleus_percentage = 0.05, nucleus_threshold = 10, shrink_nucleus = 3):
+    '''
+    Function: Adds a low probability (/intensity) to the nucleus
+    Input:    cell mask (numpy array),
+    nucleus_percentage (percentage of signal that should be in the nucleus)
+    Output:   The image with nuclear probability.
+    '''
+    # We create a mask for the nucleus
+    nucleus_smoothed = cv2.GaussianBlur(nucleus_image,(5,5),0)
+    nucleus_mask_unshrunk = nucleus_smoothed > nucleus_threshold
+    kernel = np.ones((3,3),np.uint8)
+    nucleus_mask = cv2.erode(np.array(nucleus_mask, dtype=np.uint8),kernel,iterations = shrink_nucleus)
+
+    # Calculating total signal from the cytoplasm and nucleus area.
+    total_cytoplasm = np.sum(cell_mask)
+    nucleus_area = np.sum(nucleus_mask != 0)
+
+    # Making sure nucleus intensity is a certain percentage from the overall signal.
+    cell_mask[nucleus_mask != 0] = (nucleus_percentage * total_cytoplasm)/(nucleus_area * (1 - nucleus_percentage))
+
+    return cell_mask
