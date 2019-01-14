2d72ded75d3113cecc72f6a9798a57f849d7f74d
Joel Lüthi <joel.luethi@uzh.ch>
2019-01-14 14:15:13 +0100

    Adding an input to the sampling function that allows it to reject P-bodies too close to already sampled P-bodies

--- src/p_body_randomness/sampling.py
+++ src/p_body_randomness/sampling.py
@@ -1,14 +1,19 @@
 import numpy as np
 from scipy.stats import rv_discrete
+from metrics import nearest_neighbor_distance
 
 
-def sample_pbodies(sampling_map, n, area_fn=None):
+def sample_pbodies(sampling_map, n, area_fn=None, min_distance = 10):
     '''
     Args:
         cytoplasmic_mask: Either a binary numpy mask representing the sample
             area or a intensity map representing sampling probabilities
         n: The number of pbodies to sample
         area_fn: A function to sample the p body area from or None
+        min_distance: Int. Minimal distance that a newly sampled P-body has to
+        all existing P-bodies. If it's sampled closer to an existing P-body,
+        the sampling is redone (becase P-bodies can't biologically overlap,
+        they would have fused and be treated as 1 P-body)
 
     Returns a list of n sampled positions
     '''
@@ -22,7 +27,17 @@ def sample_pbodies(sampling_map, n, area_fn=None):
         except TypeError:
             area = 0
 
-        samples.append((x, y, area))
+        # Check distance to existing P-pbodies
+        if len(samples) > 1:
+            try:
+                # Calculate the nearest neighbor distance to all other existing P-pbodies
+                min_distance_measured = min(nearest_neighbor_distance(samples))
+                if min_distance_measured > min_distance:
+                    samples.append((x, y, area))
+                # when only 2 P-bodies are present and it samples the same location,
+                # the nearest neighbor function throws a ValueError
+            except ValueError:
+                pass
 
     return samples
 
